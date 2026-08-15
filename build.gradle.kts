77714c0a42e76769f1f9aef4142b36f3ee06e763
vaceslavpereaslov <perejaslov9200@gmail.com>
2026-08-15 17:40:22 +0300

    Configure maven-publish and jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.androidLibrary)
+    `maven-publish`
 }
 
 android {
@@ -19,12 +20,20 @@ android {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     buildFeatures {
         buildConfig = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
     }
 }
 
@@ -38,4 +47,17 @@ dependencies {
     implementation(libs.androidx.core.ktx)
 
     api(files("libs/hiddify-core.aar"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.YaslikS"
+                artifactId = "easy_hiddify_lib"
+                version = "1.0.0"
+            }
+        }
+    }
 }
